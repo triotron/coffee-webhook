c83b50836a629b820220f131071fdfcedb397b6d
triotron <97348329+triotron@users.noreply.github.com>
2022-01-09 20:03:24 +0300

    Delete admin_kb.py

--- keyboards/admin_kb.py
+++
@@ -1,8 +0,0 @@
-# - *- coding: utf- 8 - *-
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-
-button_load = KeyboardButton('/Загрузить')
-button_delete = KeyboardButton('/Удалить')
-
-button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete)
-
