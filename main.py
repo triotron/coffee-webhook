94834fecd9c66a4fd96959c987cb4a2a6dbbff31
triotron <97348329+triotron@users.noreply.github.com>
2022-01-15 20:28:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ def answer(call):
         item_username=types.KeyboardButton('Мой ник')
 
         markup_reply.add(item_id, item_username)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ну что же, начнем!!!!")
+        #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ну что же, начнем!!!!")
         bot.send_message(call.message.chat.id, text='Нажмите на одну из кнопок', reply_markup=markup_reply)
 
         #bot.answer_inline_query(call.id)
 
     elif call.data == 'no':
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Но почему!!!!")
+        #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Но почему!!!!")
         bot.send_message(call.message.chat.id, 'Ну нет, так нет', reply_markup=telebot.types.ReplyKeyboardRemove())
         #bot.answer_inline_query(call.id)
 
